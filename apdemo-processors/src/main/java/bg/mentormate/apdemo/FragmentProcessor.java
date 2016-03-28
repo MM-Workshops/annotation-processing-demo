a580bc0483d03a7a2f11c7efe6e39a9213b3575d
TL Nguyen <tung.lam.nguyen@mentormate.com>
2016-03-28 18:23:59 +0300

    Adding modules for annotations and processors

---
+++ apdemo-processors/src/main/java/bg/mentormate/apdemo/FragmentProcessor.java
@@ -0,0 +1,67 @@
+package bg.mentormate.apdemo;
+
+import com.google.auto.service.AutoService;
+import com.google.common.collect.ImmutableSet;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Set;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.annotation.processing.Processor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.TypeElement;
+import javax.tools.Diagnostic;
+
+@AutoService(Processor.class)
+public class FragmentProcessor extends AbstractProcessor {
+
+    @Override
+    public synchronized void init(ProcessingEnvironment processingEnv) {
+        super.init(processingEnv);
+    }
+
+    @Override public Set<String> getSupportedAnnotationTypes() {
+        return ImmutableSet.of(
+                FragmentCreator.class.getCanonicalName());
+    }
+
+    @Override
+    public SourceVersion getSupportedSourceVersion() {
+        return SourceVersion.latestSupported();
+    }
+
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        try {
+            return doProcess(annotations, roundEnv);
+        } catch (Throwable e) {
+            StringWriter writer = new StringWriter();
+            e.printStackTrace(new PrintWriter(writer));
+            processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, writer.toString());
+            return false;
+        }
+    }
+
+    private boolean doProcess(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        for (Element annotated: roundEnv.getElementsAnnotatedWith(FragmentCreator.class) ) {
+            try {
+                scanElement(annotated);
+            } catch (Exception e) {
+                processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, e.getMessage());
+            }
+        }
+        return false;
+    }
+
+    private void scanElement(Element annotated) throws Exception {
+        // Example
+        if (annotated.getKind() != ElementKind.CLASS) {
+            throw new Exception("@FragmentCreator can only be applied to a class");
+        }
+    }
+}
---
+++ apdemo-sample/src/main/java/bg/mentormate/apdemo/MainActivity.java
@@ -0,0 +1,13 @@
+package bg.mentormate.apdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
