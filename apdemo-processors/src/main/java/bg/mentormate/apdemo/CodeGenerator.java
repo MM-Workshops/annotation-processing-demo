135acb93826ad589fbadae3ab0361c985cbd2949
TL Nguyen <tl.nguyen.1987@gmail.com>
2016-07-04 23:20:57 +0300

    Inflating views

--- apdemo-processors/src/main/java/bg/mentormate/apdemo/CodeGenerator.java
+++ apdemo-processors/src/main/java/bg/mentormate/apdemo/CodeGenerator.java
@@ -1,42 +1,66 @@
 package bg.mentormate.apdemo;
 
+import com.google.common.base.CaseFormat;
 import com.squareup.javapoet.ClassName;
 import com.squareup.javapoet.MethodSpec;
 import com.squareup.javapoet.TypeSpec;
 
+import java.util.List;
+
 import static com.squareup.javapoet.MethodSpec.methodBuilder;
 import static com.squareup.javapoet.TypeSpec.classBuilder;
 import static javax.lang.model.element.Modifier.FINAL;
+import static javax.lang.model.element.Modifier.PRIVATE;
 import static javax.lang.model.element.Modifier.PUBLIC;
 
 /**
  * Created by tung.lam.nguyen on 04.07.2016
  */
 public class CodeGenerator {
-    public static TypeSpec generateClass(AnnotatedClass annotatedClass) {
+    private static final ClassName FRAGMENT = ClassName.get("android.app", "Fragment");
+    private static final ClassName LAYOUT_INFLATER = ClassName.get("android.view", "LayoutInflater");
+    private static final ClassName VIEW_GROUP = ClassName.get("android.view", "ViewGroup");
+    private static final ClassName BUNDLE = ClassName.get("android.os", "Bundle");
+    private static final ClassName VIEW = ClassName.get("android.view", "View");
+    private static final ClassName LOG = ClassName.get("android.util", "Log");
 
+    public static TypeSpec generateClass(AnnotatedClass annotatedClass) {
+        List<String> views = annotatedClass.getViews();
         TypeSpec.Builder builder =  classBuilder("Generated" + annotatedClass.getName())
-                .addModifiers(PUBLIC, FINAL);
-        builder.superclass(ClassName.get("android.app", "Fragment"))
-                .addMethod(buildOnCreateViewMethod(annotatedClass));
+                .addModifiers(PUBLIC, FINAL)
+                .superclass(FRAGMENT)
+                .addMethod(buildOnCreateViewMethod())
+                .addMethod(buildInitViewsMethod(views));
+        for (String field : views) {
+            builder.addField(VIEW, field, PRIVATE);
+        }
         return builder.build();
     }
 
-    private static MethodSpec buildOnCreateViewMethod(AnnotatedClass annotatedClass) {
+    private static MethodSpec buildInitViewsMethod(List<String> views) {
+        final MethodSpec.Builder initViewsMethodBuilder = methodBuilder("initViews")
+                .addModifiers(PRIVATE);
+        for (String view : views) {
+            initViewsMethodBuilder.addStatement(
+                    view + " = getActivity().findViewById(R.id."+ getViewIdByName(view) +")");
+        }
+        return initViewsMethodBuilder.build();
+    }
+
+    private static String getViewIdByName(String viewName) {
+        return CaseFormat.LOWER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, viewName);
+    }
+
+    private static MethodSpec buildOnCreateViewMethod() {
         final MethodSpec.Builder onCreateViewBuilder = methodBuilder("onCreateView")
                 .addModifiers(PUBLIC)
                 .addAnnotation(Override.class)
-                .addParameter(ClassName.get("android.view", "LayoutInflater"), "inflater")
-                .addParameter(ClassName.get("android.view", "ViewGroup"), "container")
-                .addParameter(ClassName.get("android.os", "Bundle"), "savedInstanceState")
-                .returns(ClassName.get("android.view", "View"))
-                .addStatement("super.onCreateView(inflater, container, savedInstanceState)");
-        StringBuilder builder = new StringBuilder();
-        for (String s : annotatedClass.getViews()) {
-            builder.append(s);
-        }
-        onCreateViewBuilder
-                .addStatement("$T.d(\"Cool\", \""+builder.toString() +"\")", ClassName.get("android.util", "Log"))
+                .addParameter(LAYOUT_INFLATER, "inflater")
+                .addParameter(VIEW_GROUP, "container")
+                .addParameter(BUNDLE, "savedInstanceState")
+                .returns(VIEW)
+                .addStatement("super.onCreateView(inflater, container, savedInstanceState)")
+                .addStatement("initViews()")
                 .addStatement("return inflater.inflate(R.layout.fragment_fragment1, container, false)");
         return onCreateViewBuilder.build();
     }
---
+++ apdemo-processors/src/main/java/bg/mentormate/apdemo/UltimateFragment.java
@@ -0,0 +1,7 @@
+package bg.mentormate.apdemo;
+
+/**
+ * Created by TL on 7/4/2016.
+ */
+public interface UltimateFragment {
+}
